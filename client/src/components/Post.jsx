28477bbd74cc92f04b8b45b261dd1684d8540e14
Bruce-Yu2017 <ilywanzi@gmail.com>
2021-01-02 17:28:20 -0800

    add moment js to use relative time

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,43 +1,44 @@
 import React from "react";
 import { Card, Row, Col, Badge, Image } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import moment from "moment";
 
 const Post = ({ post }) => {
   return (
-    <div className="cursor-pointer">
-      <LinkContainer to={`/post/${post._id}`}>
-        <Card style={{ width: "100%" }} className="mb-2">
-          <Card.Body>
-            <Row>
-              <Col md={10}>
-                <Card.Subtitle className="mb-2 text-muted small">
-                  Post by <strong>{post.user.name}</strong> at{" "}
-                  {new Date(post.createdAt).toLocaleDateString()}{" "}
-                  {post.tags.length > 0 &&
-                    post.tags.map((tag, index) => (
-                      <Badge
-                        variant="success"
-                        pill
-                        className="ml-2"
-                        key={`${tag}${index}`}
-                      >
-                        {tag}
-                      </Badge>
-                    ))}
-                </Card.Subtitle>
-                <Card.Title>{post.name}</Card.Title>
-                <Card.Text>{post.description}</Card.Text>
-              </Col>
-              <Col md={2} className="text-right">
-                <Image
-                  src={`https://source.unsplash.com/80x80/?${post.name}`}
-                  fluid
-                />
-              </Col>
-            </Row>
-          </Card.Body>
-        </Card>
-      </LinkContainer>
+    <div>
+      <Card style={{ width: "100%" }} className="mb-2">
+        <Card.Body>
+          <Row>
+            <Col md={10}>
+              <Card.Subtitle className="mb-2 text-muted small">
+                Post by <strong>{post.user.name}</strong>{" "}
+                {moment(post.createdAt).fromNow()}{" "}
+                {post.tags.length > 0 &&
+                  post.tags.map((tag, index) => (
+                    <Badge
+                      variant="success"
+                      pill
+                      className="ml-2"
+                      key={`${tag}${index}`}
+                    >
+                      {tag}
+                    </Badge>
+                  ))}
+              </Card.Subtitle>
+              <LinkContainer to={`/post/${post._id}`}>
+                <Card.Title className="cursor-pointer">{post.name}</Card.Title>
+              </LinkContainer>
+              <Card.Text>{post.description}</Card.Text>
+            </Col>
+            <Col md={2} className="text-right">
+              <Image
+                src={`https://source.unsplash.com/80x80/?${post.name}`}
+                fluid
+              />
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
